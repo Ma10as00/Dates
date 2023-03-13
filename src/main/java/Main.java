51f7b7e0471e4ff6ceacb4e87c2ba5f776403d8a
Mathias Øgaard <mapiza00@gmail.com>
2023-03-14 12:42:52 +1300

    Clears error-file before running

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,6 +8,7 @@ public class Main {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         PrintStream errors = new PrintStream("error.txt"); 
+        errors.flush();
         System.setErr(errors); //Redirects stderr to print in error.txt instead of printing to the terminal
         ChatBot cb = new ChatBot();
         cb.startConversation();

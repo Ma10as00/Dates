26536d8e45207d8c6ccb74ca7edb231d130f4b7a
Mathias Øgaard <mapiza00@gmail.com>
2023-03-21 10:58:14 +1300

    Added some test cases

--- src/test/java/ChatBotTest.java
+++ src/test/java/ChatBotTest.java
@@ -21,14 +21,23 @@ public class ChatBotTest{
         strings.add("3-8-24");
         strings.add("14/may/67");
         strings.add("25 08 1974");
+        strings.add("29 2 1");
+        strings.add("1212-12-12");
         dates.add(new Date(3, Month.AUGUST, 2024));
         dates.add(new Date(14, Month.MAY, 1967));
         dates.add(new Date(25, Month.AUGUST, 1974));
+        dates.add(new Date(29, Month.FEBRUARY, 2001));
+        dates.add(new Date(1212, Month.DECEMBER,2012));
         printed.add("03 Aug 2024");
         printed.add("14 May 1967");
         printed.add("25 Aug 1974");
+        printed.add("29 Feb 2001 - INVALID");
+        printed.add("1212 Dec 2012 - INVALID");
     }
 
+    /**
+     * Tests that the chatbot reads the dates correctly, i.e. readDate() returns the correct date. 
+     */
     @Test
     public void readDateTest() {
         for (int i=0; i<strings.size(); i++){
